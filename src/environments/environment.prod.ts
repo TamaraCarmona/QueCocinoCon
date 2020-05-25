bd422c46bbbd973952d090a014df9e6deb6fb08f
Federico Lopez 110398 <110398@tecnicatura.frc.utn.edu.ar>
2020-05-25 00:18:49 -0300

    add font aw search

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   domain: "tamaracarmona.auth0.com",
   cliente: "jnpxhH1lXjlwdOO7T2J41CZ1yMbSS68W",
   redirect: 'http://localhost:4200/callback',
-  redirectLogout: 'http://localhost:4200/callback',
+  redirectLogout: 'http://localhost:4200/logout',
 };
